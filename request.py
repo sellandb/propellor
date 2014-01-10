4a023df848bebf571e4d37cab41a276d43ad588f
sellandb <sellandb@Benjamins-MacBook-Pro.local>
2014-01-09 19:40:12 -0500

    Created the request Object and file

---
+++ request.py
@@ -0,0 +1,18 @@
+class Request:
+
+	def __init__(self, request):
+		self.data = ""
+		temp = request.recv(100)
+		counter = 0
+		
+		while (len(temp) == 100 and counter < 82):
+			self.data += temp            
+			temp = request.recv(100)
+			counter+=1
+
+		self.headers = self.data.splitlines()
+		print self.headers[0]
+
+
+
+    
