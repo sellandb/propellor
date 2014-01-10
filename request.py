5029e7eaa938698a23f9ba16ab3d4af93784710e
sellandb <sellandb@gmail.com>
2014-01-10 14:01:57 -0500

    Added method parsing to the request

--- request.py
+++ request.py
@@ -4,14 +4,28 @@ class Request:
 		self.data = ""
 		temp = request.recv(100)
 		counter = 0
+
+		#Identify the Method of the request
+		self.method = temp.split()[0]
+		if(self.method.lower() != "get"):
+			raise UnknownMethodError(self.method)
 		
+		#pull the data off of the buffer, maximum 8K for a request
 		while (len(temp) == 100 and counter < 82):
 			self.data += temp            
 			temp = request.recv(100)
 			counter+=1
 
+		#Parse out the headers
+		#TODO: Pull out the first line and part path and HTTP version
+		#Parse headers into a dictionary
 		self.headers = self.data.splitlines()
 
 
+class UnknownMethodError(Exception):
+	def __init__(self, value):
+		self.value = value
 
+	def __str__(self):
+		return repr(self.value)
     
